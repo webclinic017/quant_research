c4c1f25edb69beae5d08d1fdb89af75f3c8a9fd4
piginzoo <piginzoo@gmail.com>
2023-02-13 20:56:32 +0800

    backup qmt scripts

---
+++ dingtou/dingtou/iquant/etf.py
@@ -0,0 +1,516 @@
+# encoding:gbk
+import logging
+import csv
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from importlib import reload
+import time
+import requests
+import yaml
+import os
+import traceback
+
+import dingtou
+from dingtou.pyramid_v2.pyramid_v2_strategy import PyramidV2Strategy
+from dingtou.pyramid_v2.position_calculator import PositionCalculator
+from dingtou.utils.utils import str2date, date2str
+
+import datetime
+
+logger = logging.getLogger(__name__)
+
+# 设置所有的参数，这些参数都是经过回测的最优的，无需调整
+# stocks = ['510500.SH','510330.SH','159915.SZ','588090.SH']
+stocks = ['588090.SH']  # TODO，暂时测试一只基金
+grid_height = 0.01
+grid_amount = 3000  # 3000元比较合适，大约115w/year，一次投6.7万，最多一次24万，常备30万，低于20万就报警《ETF定投数据统计》
+quantile_positive = 0.8
+quantile_negative = 0.2
+ma_days = 850
+
+# 设置所有的目录
+home_dir = "c:\\workspace\\iquant"
+log_dir = f"{home_dir}\\logs"
+conf_path = f"{home_dir}\\config.yml"
+trans_log_dir = f"{home_dir}\\history"
+trans_log = f"{trans_log_dir}\\transaction.csv"
+last_grid_position = f"{trans_log_dir}\\last_grid_position.json"
+
+POLICY_NAME = 'ETF定投'
+MAX_TRADE_NUM_PER_DAY = 3  # 每天每只股票最大的交易次数（买和卖）
+MIN_CASH = 200000  # 小于这个资金，就要报警了，防止资金不够
+
+
+def load_conf():
+    f = open(conf_path, 'r', encoding='utf-8')
+    result = f.read()
+    return yaml.load(result, Loader=yaml.FullLoader)
+
+
+conf = load_conf()
+
+
+def _is_realtime(context):
+    return not context.do_back_test
+
+
+def init_logger(log_level=logging.DEBUG):
+    if not os.path.exists(log_dir): os.makedirs(log_dir)
+
+    file_name = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+    file_full_path = f"{log_dir}\\{file_name}.log"
+    print("开始初始化日志：file=%r" % (file_full_path))
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level=log_level)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d P%(process)d: %(message)s')
+
+    stream_handler = logging.StreamHandler()
+    root_logger.addHandler(stream_handler)
+    print("日志：创建控制台处理器")
+
+    t_handler = logging.FileHandler(file_full_path, encoding='utf-8')
+    root_logger.addHandler(t_handler)
+    print("日志：创建文件处理器", file_full_path)
+
+    handlers = root_logger.handlers
+    for handler in handlers:
+        print(handler)
+        handler.setLevel(level=log_level)
+        handler.setFormatter(formatter)
+
+
+class Broker():
+    def __init__(self, account):
+        self.context = None
+        self.account = account
+
+    def set_context(self, context):
+        self.context = context
+
+    def get_available_cash(self):
+        account = get_trade_detail_data(self.account, A.acct_type, 'account')
+        if len(account) == 0:
+            raise Exception(f'账号{A.acct} 未登录 请检查')
+        account = account[0]
+        available_cash = int(account.m_dAvailable)
+        return available_cash
+
+    def check_max_trade(self, code):
+        """
+        为了防止程序有bug，导致1天内买入某只股票太多次，也是一种风控吧，
+        限制一只股票买入次数。
+        """
+        # 这个函数只获得当天的订单，我测试的结果
+        orders = get_trade_detail_data(self.account, 'stock', 'ORDER')
+        counter = 0
+        for order in orders:
+            if order.m_strInstrumentID == code[:6]:
+                counter += 1
+        return counter > MAX_TRADE_NUM_PER_DAY
+
+    def buy(self, code, date, amount=None, position=None):
+        """
+        http://docs.thinktrader.net/vip/pages/d0dd26/#_1-%E7%BB%BC%E5%90%88%E4%BA%A4%E6%98%93%E4%B8%8B%E5%8D%95-passorder
+        passorder是对最后一根K线完全走完后生成的模型信号在下一根K线的第一个tick数据来时触发下单交易；
+        采用quickTrade参数设置为1时，非历史bar上执行时（ContextInfo.is_last_bar()为True），只要策略模型中调用到就触发下单交易。
+        quickTrade参数设置为2时，不判断bar状态，只要策略模型中调用到就触发下单交易，历史bar上也能触发下单，请谨慎使用。
+
+        logger.debug("账号%s买入基金%s %.0f股",self.account,code,position)
+        self.context.passorder(
+         opType=23, # 股票买入，或沪港通、深港通股票买入
+         orderType=1101, # 单股、单账号、普通、股/手方式下单,
+         accountid =self.account,
+         orderCode = code,
+         prType = 3, # 下单选价类型,4：卖1价；3：卖2价
+         price = 0,
+         volume = osition,  # 多少股/手：根据 orderType 值最后一位确定 volume 的单位：单股下单时：1：股 / 手
+         quickTrade，int，设定是否立即触发下单，可选值：0：否1：是,passorder是对最后一根 K 线完全走完后生成的模型信号在下一根 K 线的第一个tick数据来时触发下单交易；采用quickTrade参数设置为1时，只要策略模型中调用到passorder交易函数就触发下单交易
+         userOrderId，string，用户自设委托 ID，可缺省不写，写的时候必须把起前面的 strategyName 和 quickTrade 参数也填写。
+                                对应 order 委托对象和 deal 成交对象中的 m_strRemark 属性，
+                                通过 get_trade_detail_data 函数或委托主推函数 order_callback 和
+                                成交主推函数 deal_callback 可拿到这两个对象信息。
+         ContextInfo=self.context)
+         无法写成这个参数形式，不知为何，只好写成不带参数提示类型的调用
+        """
+        # passorder(opType,orderType,accountid,orderCode,prType,price,volume,strategy_name,quickTrade,userOrderId,ContextInfo)
+
+        # 按照股数（不是手数）进行下单
+        # passorder(23,1101, self.account, code, 3, 0, positoin, POLICY_NAME,1,order_id,self.context)
+
+        # 按照钱数进行下单，但是因为A股有最少1手(100股)的限制，所以，函数会自动取整到100股的整数倍
+        # passorder(23,1102, self.account, code, 3, 0, amount, POLICY_NAME,1,order_id,self.context)
+
+        # 这个是风控和防止bug的需要，防止一天不停的买，每次查下当日成交订单，超过3次就不让挂单了
+        if self.check_max_trade(code):
+            msg = "挂买单失败，超过当日允许的最大交易次数[%d次]：账号%s于%s日按照卖2价，买入基金%s: %.0f元" % (
+            MAX_TRADE_NUM_PER_DAY, self.account, date2str(date), code, amount)
+            logger.info(msg)
+            weixin('detail', msg)
+            mail(f'[{POLICY_NAME}] 创建买单失败', msg)
+            return False
+
+        my_cash = self.get_available_cash()
+        if my_cash < amount:
+            msg = "挂买单失败，购买金额小于可用金额[%.2f]：账号%s于%s日按照卖2价，买入基金%s: %.0f元" % (
+            my_cash, self.account, date2str(date), code, amount)
+            logger.info(msg)
+            weixin('detail', msg)
+            mail(f'[{POLICY_NAME}] 创建买单失败', msg)
+            return False
+
+        old_postions = get_trade_detail_data(self.account, 'stock', 'position')
+        order_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        order_id = f"{order_time}_{code}_{POLICY_NAME}"
+
+        # 测试按照价格买,110元
+        # passorder(23,1102, self.account, code, 3, 0, 110, POLICY_NAME,1,order_id,self.context)
+
+        msg = "账号%s于%s日按照卖2价，买入基金%s: %.0f元" % (self.account, date2str(date), code, amount)
+        logger.info(msg)
+        weixin('detail', msg)
+        mail(f'[{POLICY_NAME}] 创建买单(仅下单)', msg)
+
+        postions = get_trade_detail_data(self.account, 'stock', 'position')
+        for p in old_postions:
+            # 买入前，打印一下旧的持仓
+            logger.info("买入前，持仓：%s : %.0f股", p.m_strInstrumentID, p.m_nVolume)
+        for p in postions:
+            # 买入后，打印一下新的持仓
+            logger.info("买入后，持仓：%s : %.0f股", p.m_strInstrumentID, p.m_nVolume)
+        return True
+
+    def sell(self, code, date, amount=None, position=None):
+
+        """
+        卖出逻辑是仓位的多少
+
+        passorder(
+         opType = 24, # 24：股票卖出，或沪港通、深港通股票卖出
+         orderType = 1101, # 单股、单账号、普通、股/手方式下单,
+         accountid = self.account,
+         orderCode = code,
+         prType = 7, # 下单选价类型：6：买1价；7：买2价
+         price = 0,
+         volume = position,
+         ContextInfo= self.context )
+        """
+
+        # 这个是风控和防止bug的需要，防止一天不停的买，每次查下当日成交订单，超过3次就不让挂单了
+        if self.check_max_trade(code):
+            msg = "挂卖单失败，超过当日允许的最大交易次数[%d次]：账号%s于%s日按照买2价，买入基金%s: %.0f元" % (
+            MAX_TRADE_NUM_PER_DAY, self.account, date2str(date), code, amount)
+            logger.info(msg)
+            weixin('detail', msg)
+            mail(f'[{POLICY_NAME}] 创建卖单失败', msg)
+            return False
+
+        postions = get_trade_detail_data(self.account, 'stock', 'position')
+        available_position = None
+        for p in postions:
+            # logger.debug("卖出：%s ：%.0f", p.m_strInstrumentID, p.m_nVolume)
+            # code是510500.SH，但是m_strInstrumentID是510500，所以只取前6位
+            if p.m_strInstrumentID == code[0:6] and p.m_nVolume < position:
+                available_position = p.m_nVolume
+            if p.m_strInstrumentID == code[0:6] and p.m_nVolume >= position:
+                available_position = position
+
+        if available_position is None:
+            logger.warning("账号%s于%s日卖出基金%s %.0f股失败：没有持仓", self.account, date2str(date), code, position)
+            return False
+
+        old_postions = get_trade_detail_data(self.account, 'stock', 'position')
+        order_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        order_id = f"{order_time}_{code}_{POLICY_NAME}"
+
+        # 按照买2价进行买入，没选买1，保险起见
+        passorder(24, 1101, self.account, code, 7, 0, available_position, POLICY_NAME, 1, order_id, self.context)
+
+        msg = "账号%s于%s日按照买2价，卖出基金%s %.0f股" % (self.account, date2str(date), code, available_position)
+        logger.info(msg)
+        weixin('detail', msg)
+        mail(f'[{POLICY_NAME}] 创建卖单', msg)
+
+        postions = get_trade_detail_data(self.account, 'stock', 'position')
+        for p in postions:
+            # 买入后，打印一下新的持仓
+            logger.info("卖出后，持仓：%s : %.0f股", p.m_strInstrumentID, p.m_nVolume)
+
+        return True
+
+
+def mail(title, msg):
+    uid = conf['email']['uid']
+    pwd = conf['email']['pwd']
+    host = conf['email']['host']
+    email = conf['email']['email']
+
+    subject = title
+    receivers = [email]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
+    message = MIMEText(msg, 'plain', 'utf-8')
+    message['From'] = Header("量化机器人", 'utf-8')  # 发送者
+    message['To'] = Header("量化机器人", 'utf-8')  # 接收者
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        # logger.info("发送邮件[%s]:[%s:%s]", host,uid,pwd)
+        smtp = smtplib.SMTP_SSL(host)
+        smtp.login(uid, pwd)
+        smtp.sendmail(uid, receivers, message.as_string())
+        logger.info("发往[%s]的邮件通知完成，标题：%s", email, title)
+        return True
+    except smtplib.SMTPException:
+        logger.exception("发往[%s]的邮件出现异常，标题：%s", email, email)
+        return False
+
+
+def weixin(name, msg):
+    """
+    接口文档：https://developer.work.weixin.qq.com/document/path/91770?version=4.0.6.90540
+    """
+    logger.info("开始推送微信[类别:%s]消息", name)
+
+    url = conf['weixin'][name]
+
+    post_data = {
+        "msgtype": "text",
+        "text": {
+            "content": msg[:2040]  # content    是    文本内容，最长不超过2048个字节，必须是utf8编码
+        }
+    }
+    headers = {'Content-Type': 'application/json'}
+    try:
+        requests.post(url, json=post_data, headers=headers)
+        logger.info("发往企业微信机器人[%s]的通知完成", name)
+        return True
+    except Exception:
+        logger.exception("发往企业微信机器人[%s]的消息，发生异常", name)
+        return False
+
+
+# 创建空的类的实例 用来保存委托状态
+# ContextInfo对象在盘中每次handlebar调用前都会被深拷贝,
+# 如果调用handlebar的分笔不是k线最后分笔 ContextInfo会被回退到深拷贝的内容
+# 所以ContextInfo不能用来记录快速交易的信号
+class a():
+    pass
+
+
+A = a()
+
+
+def init(ContextInfo):
+    logger.info('策略开始初始化...')
+
+    init_logger(logging.DEBUG)
+
+    # 不知为何，实盘无法reload包
+    if not _is_realtime(ContextInfo):
+        reload(dingtou)
+        reload(dingtou.pyramid_v2.pyramid_v2_strategy)
+        reload(dingtou.utils.utils)
+        logger.info("重新加载python包")
+
+    if not os.path.exists(trans_log_dir):
+        os.makedirs(trans_log_dir)
+
+    A.acct = conf['account']  # 账号为模型交易界面选择账号
+    A.acct_type = 'stock'  # 账号类型为模型交易界面选择账号
+    A.stock_list = stocks
+    A.data = {}
+
+    ContextInfo.trade_code_list = stocks
+    ContextInfo.set_universe(ContextInfo.trade_code_list)
+    ContextInfo.set_account(A.acct)  # 之前未设置，导致deal_callback未回调，咨询了客服才想到
+    ContextInfo.buy = True  # 必须要设置，否则，就不会触发买和卖，默认都是False
+    ContextInfo.sell = True
+
+    logger.info("设置股票池：%r", ContextInfo.trade_code_list)
+    policy = PositionCalculator(grid_amount)
+
+    A.broker = Broker(A.acct)
+    A.strategy = PyramidV2Strategy(
+        A.broker,
+        policy,
+        grid_height,
+        quantile_positive,
+        quantile_negative,
+        ma_days,
+        None,
+        last_grid_position  # 记录每只基金的最后的grid位置的json文件
+    )
+    logger.info('策略初始化完毕')
+
+
+def get_last_price(ContextInfo, stock_code):
+    """得到最后1分钟的价格(实盘)，或者最后1天的价格（回测）"""
+
+    if _is_realtime(ContextInfo):
+        period = '1m'  # 实盘的时候，取上一个bar的最后close的价格
+    else:
+        period = '1d'  # 模拟盘的时候，取上一天的close价格
+    df = ContextInfo.get_market_data(
+        fields=['close'],
+        stock_code=[stock_code],
+        count=1,
+        period=period,
+        dividend_type='back')
+
+    logger.debug("获取[%s]%s日的(%s)价格:%.2f", stock_code, df.iloc[0].name, period, df.iloc[0].item())
+
+    return df.iloc[0]
+
+
+def deal_callback(ContextInfo, dealInfo):
+    """
+    交易成功的回调
+    """
+    _date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = ['证券代码', '买/卖', '合同编号', '成交日期', '成交时间', '成交价格', '成交数量', '手续费', '成交额',
+               '备注']
+    data = [dealInfo.m_strInstrumentID,
+            dealInfo.m_nDirection,
+            dealInfo.m_strOrderSysID,
+            dealInfo.m_strTradeDate,
+            dealInfo.m_strTradeTime,
+            dealInfo.m_dPrice,
+            dealInfo.m_nVolume,
+            dealInfo.m_dComssion,
+            dealInfo.m_dTradeAmount,
+            dealInfo.m_strRemark]
+
+    logger.info("交易成功：")
+    logger.info(headers)
+    logger.info(data)
+    weixin('detail', f"[{POLICY_NAME}] 交易完成:\n{headers}\n{data}")
+    mail(f'[{POLICY_NAME}] 交易成功完成', f"{headers}\n{data}")
+
+    flag_header = False
+    if not os.path.exists(trans_log):
+        flag_header = True
+
+    with open(trans_log, "a") as f:
+        writer = csv.writer(f, lineterminator="\n")
+        if flag_header: writer.writerow(headers)
+        writer.writerow(data)
+
+
+def load_data(C, stock_code, today):
+    df = A.data.get(stock_code, None)
+
+    # 因为每分钟都会运行这个程序，所以为了性能，不是每分钟都加载，每天只加载一次
+    if df is not None and df.iloc[-1].name == today:
+        # logger.debug("[%s] %s日的数据，已经有了，无需再加载" , today, stock_code)
+        # 有今天的日期，就啥都不做了
+        return df
+
+    # 如果和今天不一样，就需要重新加载数据
+    df = C.get_market_data(
+        fields=['close'],
+        stock_code=[stock_code],
+        count=2400,  # 为了要找出很长的历史下的80%和20%分位数，需要加载十年的，其实只有8年的数据，因为有2年需要做移动平均，都是na
+        period='1d',  # 这个会获得当天的信息
+        dividend_type='front')  # 采用了前复权，主要是为了获得最新的价格
+    # 替换掉旧的数据
+    A.data[stock_code] = df
+
+    logger.info("加载%s日期%s~%s数据%d行", stock_code, df.iloc[0]._name, df.iloc[-1]._name, len(df))
+    A.strategy.set_data(df_baseline=None, funds_dict=A.data)
+    return df
+
+
+def handlebar(ContextInfo):
+    try:
+        __handlebar(ContextInfo)
+    except Exception as e:
+        msg = ''.join(list(traceback.format_exc()))
+        weixin('error', msg)
+        mail(f'[{POLICY_NAME}] 发生异常错误', msg)
+        logger.exception("handlerbar异常")
+
+
+def __handlebar(C):
+    # 实盘/模拟盘的时候，从2015开始，所以需要跳过历史k线，
+    # 回测的时候不需要
+    if _is_realtime(C) and not C.is_last_bar():
+        return
+
+    # 获得当天的日期
+    s = C.stockcode
+    d = C.barpos
+    t = C.get_bar_timetag(d)
+    date = timetag_to_datetime(t, '%Y%m%d')
+
+    # 跳过非交易时间
+    now = datetime.datetime.now()
+    now_time = now.strftime('%H%M%S')
+    if _is_realtime(C) and (now_time < '093000' or now_time > "150000"):
+        logger.warning("不在交易时间：%s", now_time)
+        # TODO，注释掉，方便测试
+        return
+
+    """
+    logger.debug("K线数量%d,bar的第一个tick:%r,K线号:%d,最后一个bar:%r",
+        C.time_tick_size,
+        C.is_new_bar(),
+        C.barpos,
+        C.is_last_bar())
+    """
+
+    # 注：在回测模式中，交易函数调用虚拟账号进行交易，在历史 K 线上记录买卖点，用以计算策略净值/
+    # 回测指标；实盘运行调用策略中设置的资金账号进行交易，产生实际委托；模拟运行模式下交易函数无
+    # 效。其中，can_cancel_order, cancel和do_order交易函数在回测模式中无实际意义，不建议使用。
+    account = get_trade_detail_data(A.acct, A.acct_type, 'account')
+    if len(account) == 0:
+        logger.warning(f'账号{A.acct} 未登录 请检查')
+        return
+
+    # 开盘后和收盘前，如果资金不足，就提醒
+    if _is_realtime(C) and (now_time[:4] == '0931' or now_time[:4] == "1459"):
+        account = account[0]
+        available_cash = int(account.m_dAvailable)
+        # logger.debug("交易日期: %s, 账号：%s,可用资金：%.2f",date, A.acct, available_cash)
+        if available_cash < MIN_CASH:
+            msg = "现金不足：当前现金 %.0f 元 < 最少的要求 %.0f 元" % (available_cash, MIN_CASH)
+            logger.warning(msg)
+            weixin('detail', msg)
+            mail(f'[{POLICY_NAME}] 请补充现金', msg)
+
+    # passorder(23,1101,A.acct,'588090.SH',3,0,100,C)
+    for stock_code in A.stock_list:
+
+        # 获得实盘上，最后一分钟的close的值
+        series_last_price = get_last_price(C, stock_code)
+
+        # 从series中，得到当天的日期
+        today = series_last_price._name[:8]  # 获得当天的日期
+
+        # 从series中，得到最后一分钟的价格
+        last_price = series_last_price.item()
+
+        # 判断是否需要重新加载数据，只有缓存的数据的最后日期和今天不一致的时候，才需要重新加载
+        df = load_data(C, stock_code, today)
+
+        # 得到最后一天的MA
+        last_ma = df.iloc[-1].ma
+
+        # 算出相差的百分比
+        diff2last = (last_price - last_ma) / last_ma
+
+        # 调试用
+        A.broker.set_context(C)
+        # last_price，是最后的后复权价格，注意是后复权
+        msg = A.strategy.handle_one_fund(stock_code, str2date(date), last_price, last_ma, diff2last)
+
+        if msg:
+            weixin('detail', f"[{POLICY_NAME}] 触发交易:\n{msg}")
+            mail(f'[{POLICY_NAME}] 触发交易', f"{msg}")
+
+
+
+
+
---
+++ dingtou/dingtou/iquant/heartbeat.py
@@ -0,0 +1,177 @@
+# encoding:gbk
+import datetime
+import logging
+import os
+import traceback
+
+import requests
+import yaml
+
+logger = logging.getLogger(__name__)
+
+# 设置所有的目录
+home_dir = "c:\\workspace\\iquant"
+log_dir = f"{home_dir}\\logs"
+conf_path = f"{home_dir}\\config.yml"
+trans_log_dir = f"{home_dir}\\history"
+trans_log = f"{trans_log_dir}\\transaction.csv"
+last_grid_position = f"{trans_log_dir}\\last_grid_position.json"
+url = "http://127.0.0.1:8080/api?token=my_secret_token@piginzoo.com"
+# url = "http://stock.piginzoo.com/api?token=my_secret_token@piginzoo.com"
+
+POLICY_NAME = '心跳'
+
+
+def load_conf():
+    f = open(conf_path, 'r', encoding='utf-8')
+    result = f.read()
+    return yaml.load(result, Loader=yaml.FullLoader)
+
+
+conf = load_conf()
+
+
+def init_logger(file_full_path, log_level=logging.DEBUG):
+    if not os.path.exists(log_dir): os.makedirs(log_dir)
+    print("开始初始化日志：file=%r" % (file_full_path))
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level=log_level)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d P%(process)d: %(message)s')
+
+    stream_handler = logging.StreamHandler()
+    root_logger.addHandler(stream_handler)
+    print("日志：创建控制台处理器")
+
+    t_handler = logging.FileHandler(file_full_path, encoding='utf-8')
+    root_logger.addHandler(t_handler)
+    print("日志：创建文件处理器", file_full_path)
+
+    handlers = root_logger.handlers
+    for handler in handlers:
+        print(handler)
+        handler.setLevel(level=log_level)
+        handler.setFormatter(formatter)
+
+
+def get_accounts():
+    results = []
+    account_info = get_trade_detail_data(A.account, 'stock', 'account')
+    for i in account_info:
+        r = {}
+        r['总资产'] = i.m_dBalance
+        r['可用金额'] = i.m_dAvailable
+        r['总市值'] = i.m_dInstrumentValue
+        r['总盈亏'] = i.m_dPositionProfit
+        results.append(r)
+    return results
+
+
+def get_positions():
+    results = []
+    position_info = get_trade_detail_data(A.account, 'stock', 'position')
+    for i in position_info:
+        r = {}
+        r['证券代码'] = i.m_strInstrumentID
+        r['证券名称'] = i.m_strInstrumentName
+        r['市场名称'] = i.m_strExchangeName
+        r['成交日期'] = i.m_strOpenDate
+        r['当前拥股'] = i.m_nVolume
+        r['持仓成本'] = i.m_dOpenPrice
+        r['成本价'] = i.m_dOpenCost
+        r['最新价'] = i.m_dSettlementPrice
+        r['盈亏'] = i.m_dFloatProfit
+        r['市值'] = i.m_dMarketValue
+        r['盈亏比例'] = i.m_dProfitRate
+        results.append(r)
+    return results
+
+
+def get_deals():
+    results = []
+    deal_info = get_trade_detail_data(A.account, 'stock', 'DEAL')
+    for i in deal_info:
+        r = {}
+        r['证券代码'] = i.m_strInstrumentID
+        r['证券名称'] = i.m_strInstrumentName
+        r['投资备注'] = i.m_strRemark
+        r['合同编号'] = i.m_strOrderSysID
+        r['订单编号'] = i.m_nRef
+        r['合约编号'] = i.m_strCompactNo
+        r['成交均价'] = i.m_dPrice
+        r['成交量'] = i.m_nVolume
+        r['成交额'] = i.m_dTradeAmount
+        results.append(r)
+    return results
+
+
+class a():
+    pass
+
+
+A = a()
+A.account = conf['account']  # 账号为模型交易界面选择账号
+A.acct_type = 'stock'  # 账号类型为模型交易界面选择账号
+
+
+def init(ContextInfo):
+    init_logger(f"{log_dir}\\heartbeart.log", logging.DEBUG)
+    logger.info('策略开始初始化...')
+
+    account = get_trade_detail_data(A.account, A.acct_type, 'account')
+    if len(account) == 0:
+        raise Exception(f'账号{A.acct} 未登录 请检查')
+
+    A.acct = conf['account']  # 账号为模型交易界面选择账号
+    A.acct_type = 'stock'  # 账号类型为模型交易界面选择账号
+    ContextInfo.set_account(A.acct)  # 之前未设置，导致deal_callback未回调，咨询了客服才想到
+
+
+def handlebar(ContextInfo):
+    try:
+        __handlebar(ContextInfo)
+    except Exception as e:
+        msg = ''.join(list(traceback.format_exc()))
+        logger.exception("handlerbar异常")
+
+
+def http_json_post(url, dict_msg):
+    logger.debug("向[%s]推送消息：%r", url, dict_msg)
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=dict_msg, headers=headers)
+    logger.info('接口返回原始报文:%r', response.text if len(response.text) < 50 else response.text[:50] + "......")
+    data = response.json()
+    logger.info('接口返回Json报文:%r', data)
+    return data
+
+
+def __handlebar(C):
+    # 实盘/模拟盘的时候，从2015开始，所以需要跳过历史k线，
+    # 回测的时候不需要
+    if not C.is_last_bar():
+        return
+
+    # 获得当天的日期
+    s = C.stockcode
+    d = C.barpos
+    t = C.get_bar_timetag(d)
+    date = timetag_to_datetime(t, '%Y%m%d')
+
+    # 跳过非交易时间
+    now = datetime.datetime.now()
+    now_time = now.strftime('%H%M%S')
+
+    """
+    logger.debug("K线数量%d,bar的第一个tick:%r,K线号:%d,最后一个bar:%r",
+        C.time_tick_size,
+        C.is_new_bar(),
+        C.barpos,
+        C.is_last_bar())
+    """
+
+    http_json_post(url, {'action': 'heartbeat',
+                         'name': 'heartbeat',
+                         'info': [
+                             {'name':'accounts', 'data':get_accounts()},
+                             {'name':'positions', 'data':get_positions()},
+                             {'name':'deals', 'data':get_deals()}
+                         ]})
